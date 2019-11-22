847ba1c552e081d016cc668e7c6183c043c324b6
Valentyn Tykhomyrov <khomyart@gmail.com>
2019-11-22 12:01:44 +0200

    login page

---
+++ file_creation.php
@@ -0,0 +1,13 @@
+<?php 
+    include "header.php";
+?>
+
+
+<div class="container vh-100 p-0">
+
+</div>
+
+
+<?php 
+    include "footer.php";
+?>
--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 ?>
 
 
-
+<div class="container vh-100 p-0">
+    <div class="d-flex flex-column justify-content-center align-items-center w-100 h-100">
+        <form method="get" action="index.php" class="d-flex flex-column justify-content-center align-items-center" >
+            <input class="form-control" type="text" placeholder="Login" name=login value="">
+            <input class="form-control" type="text" style="margin-top:15px;" placeholder="Password" name=password value="">
+            <button type="submit" style="margin-top:20px;" class="btn btn-primary">Log in</button>
+        </form>
+    </div>
+</div>
 
 
 <?php 
